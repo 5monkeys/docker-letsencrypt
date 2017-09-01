516dc57344ea47dd0153b798c93312816df42833
Jonas Lundberg <jonas@5monkeys.se>
2017-09-01 09:26:44 +0200

    Respect snakeoil cert when generating

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,7 @@
 set -e
 
 # Generate certificates if not exists, i.e. first time
-if [ ! -d "/etc/letsencrypt/live" ]; then
-  mkdir -p /etc/letsencrypt/challenge
-
+if [ ! -d "/etc/letsencrypt/challenge" ]; then
   if [ ${LETSENCRYPT_DOMAINS} ]; then
     echo "Generating certficates..."
     certbot certonly \
@@ -16,25 +14,11 @@ if [ ! -d "/etc/letsencrypt/live" ]; then
       --domains ${LETSENCRYPT_DOMAINS} && \
     rm /etc/letsencrypt/ssl.conf
   else
-    echo "WARNING: Missing env vars LETSENCRYPT_EMAIL and LETSENCRYPT_DOMAINS"
+    echo "WARNING: Missing LETSENCRYPT_DOMAINS env var, skipping cert gen."
   fi
 fi
 
-# Generate nginx include conf
-if [ ! -f "/etc/letsencrypt/ssl.conf" ]; then
-  fullchain=$(find /etc/letsencrypt/live -name fullchain.pem 2> /dev/null | head -1)
-
-  if [ $fullchain ]; then
-    echo "Generating nginx config..."
-    privkey=$(dirname ${fullchain})/privkey.pem
-
-    echo "ssl_certificate    ${fullchain};" > /etc/letsencrypt/ssl.conf
-    echo "ssl_certificate_key ${privkey};" >> /etc/letsencrypt/ssl.conf
-  else
-    echo "WARNING: nginx config not generated, no certificate found"
-  fi
-fi
-
+# Generate snakeoil certificate
 if [ ! -d "/etc/letsencrypt/snakeoil" ]; then
     echo "Generating snakeoil certificate..."
     mkdir -p /etc/letsencrypt/snakeoil
@@ -44,8 +28,24 @@ if [ ! -d "/etc/letsencrypt/snakeoil" ]; then
         -x509 \
         -days 3650 \
         -subj "/C=AU/ST=Some-State/O=Internet Widgits Pty Ltd" \
-        -out /etc/letsencrypt/snakeoil/nginx.crt \
-        -keyout /etc/letsencrypt/snakeoil/nginx.key
+        -out /etc/letsencrypt/snakeoil/cert.pem \
+        -keyout /etc/letsencrypt/snakeoil/key.pem
+fi
+
+# Generate nginx include conf
+if [ ! -f "/etc/letsencrypt/ssl.conf" ]; then
+  privkey=/etc/letsencrypt/snakeoil/key.pem
+  fullchain=$(find /etc/letsencrypt/live -name fullchain.pem 2> /dev/null | head -1)
+
+  if [ ${fullchain} ]; then
+    privkey=$(dirname ${fullchain})/privkey.pem
+  else
+    fullchain=/etc/letsencrypt/snakeoil/cert.pem
+  fi
+
+  echo "Generating nginx config..."
+  echo "ssl_certificate    ${fullchain};" > /etc/letsencrypt/ssl.conf
+  echo "ssl_certificate_key ${privkey};" >> /etc/letsencrypt/ssl.conf
 fi
 
 exec "$@"
